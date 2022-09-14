804d69acda7d0f14a5f0dd6b1a7e3ff1f52833ea
David Sands <davidmaxsands@gmail.com>
2022-09-14 01:39:17 -0400

    my attempt

---
+++ StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if(numbers == "")
+        {
+            return 0;
+        }
+        if(numbers.Length == 1)
+        {
+            return int.Parse(numbers);
+        }
+        if(numbers.StartsWith("//"))
+        {
+            var result = 0;
+            var split = numbers.Remove(0,4).Split(numbers.Substring(2, 1));
+
+            foreach (var number in split)
+            {
+                var num = int.Parse(number);
+                result += num;
+            }
+
+            return result;
+        }
+        else
+        {
+
+            var result = 0;
+            Char[] delimiters = { '\n', ',' };
+            var split = numbers.Split(delimiters);
+
+            foreach(var number in split)
+            {
+                var num = int.Parse(number);
+                result += num;
+            }
+
+           return result;
+        }
+    }
+}
---
+++ StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleDigitReturnsItslef()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("1");
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void TwoOrMoreDigitReturnSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("1,6");
+
+        Assert.Equal(7, result);
+    }
+
+    [Fact]
+
+    public void NewLineSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("1,6\n6");
+
+        Assert.Equal(13, result);
+    }
+
+    [Fact]
+    public void DefineDelimiter()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//;\n1;2");
+
+        Assert.Equal(3, result);
+    }
+}
